993c44e891c93b86e6c16b5a7ff6c59f459c9067
nhokgames1 <nhok.games1@gmail.com>
2017-02-17 16:28:34 +0700

    Ok form
    Yeah

--- src/main/webapp/resources/js/demoform.js
+++ src/main/webapp/resources/js/demoform.js
@@ -29,7 +29,7 @@
       $builder.addFormObject('default', {
         component: 'sampleInput'
       });
-     
+
 
 
       $http({
@@ -38,27 +38,39 @@
                 data    : $scope.user, //forms user object
                 headers : {'Content-Type': 'application/json;charset=UTF-8'}
       }).then(function success(response){
+        $scope.daa = 
+        console.log(response.data);
+
           $scope.test= response.data[6].subject;
-          $scope.first=$scope.test.replace("[","");
-          $scope.last=$scope.first.replace("]","");
+          $scope.a=$scope.test.replace("[","");
+          $scope.b=$scope.a.replace("]","");
           // console.log($scope.last);
+
           $scope.array= [];
           $scope.array= angular.fromJson($scope.test);
 
           $scope.hehe = JSON.stringify($scope.array[0]);
           console.log($scope.hehe);
-          console.log($scope.array.length);
+          $scope.data=angular.fromJson($scope.hehe);
+          // console.log($scope.array.length);
+          // console.log(angular.isObject($scope.hehe));
+          // for (i=0;i<$scope.array.length;i++) {
+          //   $builder.addFormObject('test',$scope.hehe);
+          // }
           for (i=0;i<$scope.array.length;i++) {
-            $builder.addFormObject('test',$scope.hehe);
+            $builder.addFormObject('test',angular.fromJson(JSON.stringify($scope.array[i])));
           }
+         
+           $builder.addFormObject('test',$scope.data);
+           console.log(angular.isObject($scope.data));
 
       })
       
-      $builder.addFormObject('test',{"component":"textInput","editable":true,"index":0,"label":"Hi U","description":"Full Name","placeholder":"Tên của bạn","options":[],"required":false,"validation":"/.*/"});
-      $builder.addFormObject('test',{"component":"radio","editable":true,"index":1,"label":"Radio",
-        "description":"description",
-        "placeholder":"placeholder",
-        "options":["value one","value two"],"required":false,"validation":"/.*/"});
+      // $builder.addFormObject('test',{"component":"textInput","editable":true,"index":0,"label":"Hi U","description":"Full Name","placeholder":"Tên của bạn","options":[],"required":false,"validation":"/.*/"});
+      // $builder.addFormObject('test',{"component":"radio","editable":true,"index":1,"label":"Radio",
+      //   "description":"description",
+      //   "placeholder":"placeholder",
+      //   "options":["value one","value two"],"required":false,"validation":"/.*/"});
 
           
       $scope.form = $builder.forms['default'];
@@ -68,36 +80,36 @@
       $scope.defaultValue = {};
       $scope.defaultValue[textbox.id] = 'default value';
       $scope.defaultValue[checkbox.id] = [true, true];
-      $scope.add = function(){
-        // console.log($scope.input);
-        $scope.var =JSON.stringify($scope.input[3]);
-        $scope.count = $scope.input.length;
-        for (i=0; i<$scope.count;i ++) {
-          $builder.addFormObject('test',JSON.stringify($scope.input[i]));
+      // $scope.add = function(){
+      //   // console.log($scope.input);
+      //   $scope.var =JSON.stringify($scope.input[3]);
+      //   $scope.count = $scope.input.length;
+      //   for (i=0; i<$scope.count;i ++) {
+      //     $builder.addFormObject('test',JSON.stringify($scope.input[i]));
           
-        }
-        // for (i=0 ,i< $scope.var.length)
-        // $scope.array=angular.toJson($scope.input);
+      //   }
+      //   // for (i=0 ,i< $scope.var.length)
+      //   // $scope.array=angular.toJson($scope.input);
         
        
         
-        // $scope.user={};
-        // $scope.user.name= 'Hi';
-        // $scope.user.subject= $scope.array;
+      //   // $scope.user={};
+      //   // $scope.user.name= 'Hi';
+      //   // $scope.user.subject= $scope.array;
         
-        // $scope.first = $scope.user.subject.replace("[","");
-        // $scope.last= $scope.first.replace("]","");
-        // console.log($scope.last[0]);
+      //   // $scope.first = $scope.user.subject.replace("[","");
+      //   // $scope.last= $scope.first.replace("]","");
+      //   // console.log($scope.last[0]);
         
 
-        // $http({
-        //   method: 'POST',
-        //         url     : '/Student/add',
-        //         data    : $scope.user, //forms user object
-        //         headers : {'Content-Type': 'application/json;charset=UTF-8'}
-        // })
+      //   // $http({
+      //   //   method: 'POST',
+      //   //         url     : '/Student/add',
+      //   //         data    : $scope.user, //forms user object
+      //   //         headers : {'Content-Type': 'application/json;charset=UTF-8'}
+      //   // })
        
-       }
+      //  }
 
       return $scope.submit = function() {
         console.log($scope.form);
